e6bd549a4379f77374a38cde1bc81611b01b242f
Roman Yakovlev <tensegrity.666@icloud.com>
2019-08-01 15:47:43 +0300

    Добавляет скрипт всплывающего окна с картой, подключает скрипты

---
+++ js/map.js
@@ -0,0 +1,35 @@
+var mapPopup = document.querySelector(".modal-map");
+var mapOverlay = document.querySelector(".modal-overlay");
+var mapLink = document.querySelector(".js-map-link");
+var mapButton = document.querySelector(".js-map-btn");
+var mapClose = document.querySelector(".js-map-close");
+
+mapLink.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  mapOverlay.classList.add("modal-show-map");
+  mapPopup.classList.add("modal-show-map");
+});
+
+mapButton.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  mapOverlay.classList.add("modal-show-map");
+  mapPopup.classList.add("modal-show-map");
+});
+
+mapClose.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  mapOverlay.classList.remove("modal-show-map");
+  mapPopup.classList.remove("modal-show-map");
+});
+
+
+window.addEventListener("keydown", function(evt) {
+  if (evt.keyCode === 27) {
+
+    if (mapPopup.classList.contains("modal-show-map")) {
+        evt.preventDefault();
+        mapOverlay.classList.remove("modal-show-map");
+        mapPopup.classList.remove("modal-show-map");
+    }
+  }
+});
